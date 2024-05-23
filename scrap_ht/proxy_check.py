b107e8c7fd2280f149a7985ea53e71f943ab0822
kylych-dev <mirbekov1kylych@gmail.com>
2024-05-23 22:29:08 +0600

    proxies https problems

--- scrap_ht/proxy_check.py
+++ scrap_ht/proxy_check.py
@@ -7,7 +7,7 @@ with open('scrap_ht/valid_proxies.txt', 'r') as file:
 site_to_check = [
     'http://books.toscrape.com/',
     'https://www.okeydostavka.ru/msk',
-    'https://www.okeydostavka.ru/msk/kantseliarskie-tovary-knigi'
+    # 'https://www.okeydostavka.ru/msk/kantseliarskie-tovary-knigi'
 ]
 
 counter = 0
@@ -18,12 +18,29 @@ for site in site_to_check:
         res = requests.get(site, proxies={
                                 'http': proxies[counter],
                                 'https': proxies[counter]
-                            })
-                                    # timeout=5)
-        print(res.status_code, 'status code -=-=-=-=')
+                            },
+                                timeout=5)
+        print(res.status_code, f'status code -=-=-=-=   {proxies}   site: {site}')
+    except requests.exceptions.ProxyError as e:
+        print(f'ProxyError for proxies {proxies}: {e}')
+    except requests.exceptions.ConnectionError as e:
+        print(f'ConnectionError for proxies {proxies}: {e}')
+    except requests.exceptions.Timeout as e:
+        print(f'TimeoutError for proxies {proxies}: {e}')
     except Exception as e:
-        print(e)
-    finally:
-        counter += 1
-        if counter == len(proxies):
-            break
+        print(f'An error occurred for proxies {proxies}: {e}')
+
+    # except Exception as e:
+    #     print(e)
+    # finally:
+    #     counter += 1
+    #     if counter == len(proxies):
+    #         break
+
+
+
+
+'''
+
+complete Java code that supports both HTTP and HTTPS requests using SOCKS proxy.
+'''
