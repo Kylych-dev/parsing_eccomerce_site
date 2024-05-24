7b884ea73e8c56e31fbefd2fc39f2ab37457f269
kylych-dev <mirbekov1kylych@gmail.com>
2024-05-24 22:22:40 +0600

    proxy

--- scrap_ht/main.py
+++ scrap_ht/main.py
@@ -4,9 +4,19 @@ import re, os, click, requests
 import time, requests
 
 
-def select_store(url: str, headers: dict, proxies: dict) -> str:
-    response = requests.get(url=url, proxies=proxies, headers=headers)
+with open('scrap_ht/valid_proxies.txt', 'r') as file:
+    proxies = file.read().split('\n')
+
+
+def select_store(store_address: str, headers: dict, proxy: str) -> str:
+    proxy_dict = {
+        'http': f'http://{proxy}',
+        'https': f'https://{proxy}'
+    }
+    response = requests.get(url=store_address, proxies=proxy_dict, headers=headers)
     soup = bs(response.text, 'lxml')
+
+    url = 'cool yes!!!'
     return url
 
 def parse_category(url:str, category:str, pages:int, headers:dict) -> list:
@@ -47,18 +57,39 @@ def main_func(store_address, categories, pages):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0'
     }
-    proxies = {
-        'http': '102.214.166.1:1976',
-        'https': '102.214.166.1:1976'
-    }
+
+    # proxies = {
+    #     # 'http': '102.214.166.1:1976',
+    #     'https': 'https://102.214.166.1:1976'
+    # }
 
 
+
+    working_proxy = None
+    for proxy in proxies:
+        try:
+            print(f'Checkign proxy: {proxy}')
+            current_url = select_store(store_address, headers, proxy)
+            if current_url:
+                working_proxy = proxy
+                break
+        except requests.exceptions.RequestException as ex:
+            print(f'Error with proxies {proxy}: {ex}')
+            continue
+    if working_proxy is None:
+        print('No working proxies found.')
+        return
+
+    # current_url = select_store(store_address, headers, proxies[counter])
+
+
+    products = parse_category(current_url, categories, pages, headers)
     next_filename = get_next_filename(output_directory, base_name, extension)
 
-    res2 = select_store(store_address)
+    # res2 = select_store(store_address)
     with open(os.path.join(output_directory, next_filename), 'w', encoding='utf-8') as file:
-        for part in res2:
-            file.write(part + '\n')
+        for product in products:
+            file.write(product + '\n')
 
 if __name__ == '__main__':
-    main_func('https://www.okeydostavka.ru/msk', '/kantseliarskie-tovary-knigi', 'pages')
+    main_func('https://samokat.ru/', '/category/90b0e15a-208c-4264-ac00-07b9cff26bba/', 'pages')
